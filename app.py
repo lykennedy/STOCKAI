ffa43917b002a151ce14efffea79637bd005f253
kennedy <kennedyly01@gmail.com>
2020-05-13 20:19:59 -0400

    Added python file for starting app

---
+++ app.py
@@ -0,0 +1,6 @@
+from tensorflow.keras.datasets import fashion_mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.utils import to_categorical
+
+print("Hello There, we are able to import said classes")
